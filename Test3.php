9551292b8dd3e810d306188f8abebcbc270bbb9f
hpf0004 <hpf0004@mix.wvu.edu>
2021-01-26 18:02:17 -0500

    initial

---
+++ Test3.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
+
+git branch -M main
+git remote add origin git@github.com:hpf0004/cs230-lab.git
+git push -u origin main
