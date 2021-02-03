d33db142f14798a91a013eb573ee40275d8edb19
hpf0004 <hpf0004@mix.wvu.edu>
2021-02-02 20:17:16 -0500

    Lab 2: About Page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1 style = "color: gold">About Us</h1>
+        <p style = "color:gold">Welcome to the Muggle World!</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/arthur.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p>Arthur Weasley</p>
+                    <p>Email: aweasley@muggle.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/luna.jpeg">
+                <div class="container">
+                    <h2>CTO</h2>
+                    <p>Luna Lovegood</p>
+                    <p>Email: looney@muggle.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/neville.jpeg">
+                <div class="container">
+                    <h2>CMO</h2>
+                    <p>Neville Longbottom</p>
+                    <p>Email: plantlover@muggle.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/dobby.jpeg">
+                <div class="container">
+                    <h2>Art Designer</h2>
+                    <p>Dobby</p>
+                    <p>Email: house.elf@muggle.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
