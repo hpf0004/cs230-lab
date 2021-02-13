feedc34b6062ecf5a096c6306f8547b56e983cc9
hpf0004 <hpf0004@mix.wvu.edu>
2021-02-12 19:51:45 -0500

    Lab 3: Take 2

--- signup.php
+++ signup.php
@@ -14,25 +14,25 @@ require "includes/header.php"
                         <div class="form-group">
                             <div class="row">
                                 <div class="col">
-                                    <input type="text" class="form-control" name="fname" placeholder="First Name" required autofocus>
+                                    <input type="text" class="form-control" name="fname" placeholder="First Name" required="required" autofocus>
                                 </div>
                                 <div class="col">
-                                    <input type="text" class="form-control" name="lname" placeholder="Last Name" required autofocus>
+                                    <input type="text" class="form-control" name="lname" placeholder="Last Name" required="required" autofocus>
                                 </div>
                             </div>
                         </div>
-                        <input type="text" class="form-control" name="uname" placeholder="Username" required autofocus>
+                        <input type="text" class="form-control" name="uname" placeholder="Username" required="required" autofocus>
 
                         <label for="inputEmail" class="visually-hidden">Email address</label>
-                        <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required
+                        <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required="required"
                             autofocus>
 
                         <label for="inputPassword" class="visually-hidden">Password</label>
-                        <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required>
+                        <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required="required">
                         
                         <label for="inputPassword" class="visually-hidden">Confirm Password</label>
                         <input type="password" id="inputPass" class="form-control" name="con-pwd" placeholder="Confirm Password"
-                            required>
+                        required="required">
                            
                         <button class="w-100 btn btn-lg btn-primary" name="signup-submit" type="submit">Sign up</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2021-9999</p>
