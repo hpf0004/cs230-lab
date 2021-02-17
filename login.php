78913afc7612d689b62a27fe0aa0d949cadb9f1c
hpf0004 <hpf0004@mix.wvu.edu>
2021-02-17 18:49:46 -0500

    Lab 4: login

---
+++ login.php
@@ -0,0 +1,55 @@
+<?php
+require  "includes/header.php";
+?>
+
+<main>
+    <link rel="stylesheet" href="css/login.css">
+
+    <div class="bg-cover">
+        <div class="container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="images/c1.jpeg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/c2.jpeg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="images/c3.jpeg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+
+        <div class="h-40 center-me">
+            <div class="my-auto">
+                <form class ="form-signin" action="includes/login-helper.php" method="post">
+                    <h1 class="h3 mb-3 fw-normal" style="color:white">Please login</h1>
+                    <p class="hint-text">Welcome Back, Muggle!</p>
+                    <input type="text" class="form-control" name="uname-email" placeholder="Username/Email" required="required"
+                        autofocus>
+
+                    <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password"
+                        required="required">
+
+                    <button class="w-100 btn btn-lg btn-primary" style="background-color:green; border:none" name="login-submit" type="submit">Sign in</button>
+                    <p class="mt-5 mb-3 text-muted">&copy; 2021-9999</p>
+                </form>
+            </div>
+        </div>
+    </div>
+</main>
--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@ require "includes/header.php"
             <div class="my-auto">
                 <div class="signup-form">
                     <form action="includes/signup-helper.php" method="post">
-                        <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+                        <h1 class="h3 mb-3 fw-normal" style="color:white">Please sign up</h1>
                         <p class="hint-text">Create you account!</p>
                         <div class="form-group">
                             <div class="row">
@@ -22,19 +22,15 @@ require "includes/header.php"
                             </div>
                         </div>
                         <input type="text" class="form-control" name="uname" placeholder="Username" required="required" autofocus>
-
-                        <label for="inputEmail" class="visually-hidden">Email address</label>
+>
                         <input type="email" id="inputEmail" class="form-control" name="email" placeholder="Email address" required="required"
                             autofocus>
-
-                        <label for="inputPassword" class="visually-hidden">Password</label>
                         <input type="password" id="inputPassword" class="form-control" name="pwd" placeholder="Password" required="required">
                         
-                        <label for="inputPassword" class="visually-hidden">Confirm Password</label>
                         <input type="password" id="inputPass" class="form-control" name="con-pwd" placeholder="Confirm Password"
                         required="required">
                            
-                        <button class="w-100 btn btn-lg btn-primary" name="signup-submit" type="submit">Sign up</button>
+                        <button class="w-100 btn btn-lg btn-primary" style="background-color:green; border:none" name="signup-submit" type="submit">Sign up</button>
                         <p class="mt-5 mb-3 text-muted">&copy; 2021-9999</p>
                     </form>
                 </div>
