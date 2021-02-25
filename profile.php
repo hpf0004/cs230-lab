5bb241b97b389020c1e1e7aa0c81a53610201d6e
hpf0004 <hpf0004@mix.wvu.edu>
2021-02-25 10:21:52 -0500

    Lab 5: Profile page

---
+++ profile.php
@@ -0,0 +1,57 @@
+<?php
+    require 'includes/header.php';
+    require 'includes/dbhandler.php';
+?>
+
+<main>
+    <link rel="stylesheet" href="css/profile.css">
+    <script>
+    function triggered() {
+        document.querySelector("#prof-image").click();
+    }
+
+    function preview(e) {
+        if (e.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                document.querySelector('#prof-display').setAttribute('src', e.target.result);
+            }
+            reader.readAsDataURL(e.files[0]);
+        }
+    }
+    </script>
+
+    <?php
+    if(isset($_SESSION['uid'])) {
+        $prof_user = $_SESSION['uname']; // username after login
+        $sqlpro = "SELECT * FROM profiles WHERE uname='$prof_user';";
+        $res = mysqli_query($conn, $sqlpro);
+        $row = mysqli_fetch_array($res);
+        $photo = $row['profilepic']; // path to the profile picture
+?>
+
+    <div class="h-50 center-me text-center">
+        <div class="my-auto">
+            <form action="includes/upload-helper.php" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <img src="<?php echo $photo;?>" alt="profile pic" onclick="triggered();" id="prof-display">
+                    <input type="file" name="prof-image" id="prof-image" onchange="preview[this]" class="form-control"
+                        style="display: none;">
+                </div>
+                <label for="prof-image" id="uname-style"><?php echo $prof_user?></label>
+                <div class="form-group">
+                    <textarea name="bio" id="bio" cols="30" rows="10" placeholder="bio..."
+                        style="text-align: center;"></textarea>
+                </div>
+                <div class="form-group">
+                    <button type="submit" name="prof-submit"
+                        class="btn btn-outline-primary btn-large btn-block">Upload</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php
+    }
+?>
+</main>
